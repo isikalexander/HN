a6e0e30cbf8520af8ce35c870d9660d5da90c944
Steffan Harmaajarvi <steffanharmaajarvi@icloud.com>
2021-06-12 21:53:25 +1100

    Initial Commit

---
+++ HN/HNApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNApp.swift
+//  HN
+//
+//  Created by Steffan Harmaajarvi on 12.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
